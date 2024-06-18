c718a4e1be7ec31cff8514b0fe5c2869c3ebc35e
abiodun603 <abioduncsc@gmail.com>
2024-06-18 09:55:11 +0100

    feat: creat cart, cartItem model and assoication using sequelize

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const sequelize = require('./util/database');
 
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -42,8 +44,13 @@ Product.belongsTo(User, {
 
 User.hasMany(Product);
 
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+
 sequelize
-  .sync()
+  .sync({ force: true })
   .then((result) => {
     return User.findByPk(1);
   })
