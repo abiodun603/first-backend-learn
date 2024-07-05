29c39c4a5c626f71679bc3c0affd8077d7d628d1
abiodun603 <abioduncsc@gmail.com>
2024-07-05 13:46:12 +0100

    feat: get a single product using mongoose

--- routes/shop.js
+++ routes/shop.js
@@ -6,7 +6,7 @@ router.get('/', shopController.getIndex);
 // router.get('/cart', shopController.getCart);
 // router.post('/cart', shopController.postCart);
 router.get('/products', shopController.getProducts);
-// router.get('/products/:productId', shopController.getProduct);
+router.get('/products/:productId', shopController.getProduct);
 // router.get('/orders', shopController.getOrders);
 // router.post('/card-delete-item', shopController.postCartDeleteProduct);
 // router.post('/create-order', shopController.postOrder);
